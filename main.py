11fe00d81cebcb0849443a0eae691eca81c57309
aatec13 <saganzuzia@gmail.com>
2025-06-11 23:27:59 +0200

    Task2

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def parse_arguments():
     parser.add_argument("input_file", help="Path to the input file")
     parser.add_argument("output_file", help="Path to the output file")
     return parser.parse_args()
+
+def load_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except json.JSONDecodeError as e:
+        print(f"Error- JSON: {e}")
+        return None
