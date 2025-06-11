3a06dd322b922449b426f5bedf4ed134160ba466
aatec13 <saganzuzia@gmail.com>
2025-06-11 23:41:58 +0200

    main

--- main.py
+++ main.py
@@ -54,3 +54,37 @@ def save_xml(file_path, root):
         tree.write(file_path)
     except Exception as e:
         print(f"Error- save to XML: {e}")
+        
+def convert(input_file, output_file):
+    input_extension = input_file.split('.')[-1]
+    output_extension = output_file.split('.')[-1]
+
+    if input_extension == "json":
+        data = load_json(input_file)
+    elif input_extension in ["yml", "yaml"]:
+        data = load_yaml(input_file)
+    elif input_extension == "xml":
+        data = load_xml(input_file)
+    else:
+        print("Unsupported input format")
+        return
+    
+    if data is None:
+        print("Error reading the input file")
+        return
+    
+    if output_extension == "json":
+        save_json(output_file, data)
+    elif output_extension in ["yml", "yaml"]:
+        save_yaml(output_file, data)
+    elif output_extension == "xml":
+        save_xml(output_file, data)
+    else:
+        print("Unsupported output format")
+
+def main():
+    args = parse_arguments()
+    convert(args.input_file, args.output_file)
+
+if __name__ == "__main__":
+    main()
